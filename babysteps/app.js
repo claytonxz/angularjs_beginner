b519416d2a4029c2e595941cda00c5d1c20c9a84
joanne <jojoannexo@gmail.com>
2015-06-07 17:04:58 +0530

    add more gems

--- babysteps/app.js
+++ babysteps/app.js
@@ -1,13 +1,23 @@
 (function () {
 	var app = angular.module('store', []); 
 	app.controller('StoreController', function(){
-		this.product = gem; 
+		this.products = gems; 
+
 	});
 
-	var gem = {
+	var gems = [
+	{
 		name: 'dodecahedron',
 		price: 2.95,
 		description: '...', 
-	}
+		canPurchase: true,
+		soldOut : true, 
+	}, 
+	{
+		name: 'pentagonal gem', 
+		price: 5.95,
+		description: "...", 
+		canPurchase: false, 
+	}];
 
 })(); 
