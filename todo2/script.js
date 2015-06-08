9a3ddcd317b674fe49b9d21a19f47a7fb73d68eb
joanne <jojoannexo@gmail.com>
2015-06-08 17:19:37 +0530

    <3

--- todo2/script.js
+++ todo2/script.js
@@ -1,5 +1,24 @@
 var app = angular.module('Todo', []);
 app.controller('TodoController', function($scope) {
-	$scope.message='Angular is really really cool :D'; 
+	// $scope.original_message='Angular is really really cool :D'; 
+	$scope.todos=[
+		'Get good at Angular.', 
+		'Actually get good at Angular.', 
+		'No, seriously. Get good at Angular.'
+	]; 
+
+	$scope.done = function(todo) {
+		var indexOf = $scope.todos.indexOf(todo); 
+		if (indexOf !== -1) {
+			$scope.todos.splice(indexOf, 1);
+		}
+	}; 
+
+	$scope.add = function(e) {
+		if (e.which && e.which === 13) {
+			$scope.todos.push($scope.newTodo);
+			$scope.newTodo=''; 
+		}
+	}
 
 }); 
